c5cc24c47823fcc12af987eabf1075e8633320a2
Will Johnson <william.c1.johnson@gmail.com>
2017-10-18 11:40:56 -0400

    - Update integration test. Now working, but saw one failure.

--- IML/DeviceScannerDaemon/IntegrationTest.fs
+++ IML/DeviceScannerDaemon/IntegrationTest.fs
@@ -14,12 +14,12 @@ testList "Info Event" [
     let data = "{ \"ACTION\": \"info\" }";
 
     promise {
-      // let! destroyResult = vagrantDestroy()
-      // printfn "Finished destroying vagrant node with result %s" destroyResult
-      // let! startResult = vagrantStart()
-      // printfn "Finished loading vagrant box"
-      // let! socatResult = vagrantRunCommand "yum install -y socat"
-      // printfn "socatResult %s" socatResult
+      let! destroyResult = vagrantDestroy()
+      printfn "Finished destroying vagrant node with result %s" destroyResult
+      let! startResult = vagrantStart()
+      printfn "Finished loading vagrant box"
+      let! socatResult = vagrantRunCommand "yum install -y socat"
+      printfn "socatResult %s" socatResult
       let! triggerResult = vagrantRunCommand "udevadm trigger"
       let! socatCommandResult = vagrantPipeToShellCommand (sprintf "echo '%s'" data) ("socat - UNIX-CONNECT:/var/run/device-scanner.sock")
       printfn "socatCommandResult = %s" socatCommandResult
@@ -30,7 +30,154 @@ testList "Info Event" [
 
   yield! testFixtureAsync withSetup [
     "should call end", fun (startResult) ->
-      let expectedDevices = "{\"/devices/virtual/block/dm-0\":{\"ACTION\":\"add\",\"MAJOR\":\"253\",\"MINOR\":\"0\",\"DEVLINKS\":\"/dev/VolGroup00/LogVol00 /dev/disk/by-id/dm-name-VolGroup00-LogVol00 /dev/disk/by-id/dm-uuid-LVM-O2bUHhajc4BpdGCKOh7H5ptegIZgHJHMk4JDAKL18JIdJgjwOsuEP33DfQDYrQqb /dev/disk/by-uuid/c3af9192-4bb5-45e5-b818-1137996a4c48 /dev/mapper/VolGroup00-LogVol00\",\"PATHS\":[\"/dev/dm-0\",\"/dev/VolGroup00/LogVol00\",\"/dev/disk/by-id/dm-name-VolGroup00-LogVol00\",\"/dev/disk/by-id/dm-uuid-LVM-O2bUHhajc4BpdGCKOh7H5ptegIZgHJHMk4JDAKL18JIdJgjwOsuEP33DfQDYrQqb\",\"/dev/disk/by-uuid/c3af9192-4bb5-45e5-b818-1137996a4c48\",\"/dev/mapper/VolGroup00-LogVol00\"],\"DEVNAME\":\"/dev/dm-0\",\"DEVPATH\":\"/devices/virtual/block/dm-0\",\"DEVTYPE\":\"disk\",\"ID_VENDOR\":null,\"ID_MODEL\":null,\"ID_SERIAL\":null,\"ID_FS_TYPE\":\"xfs\",\"ID_PART_ENTRY_NUMBER\":null,\"IML_SIZE\":\"78577664\",\"IML_SCSI_80\":\"SATA     VBOX HARDDISK   VB600bbe13-4145b7ad\",\"IML_SCSI_83\":\"1ATA     VBOX HARDDISK                           VB600bbe13-4145b7ad\",\"IML_IS_RO\":false},\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda\":{\"ACTION\":\"add\",\"MAJOR\":\"8\",\"MINOR\":\"0\",\"DEVLINKS\":\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad /dev/disk/by-path/pci-0000:00:01.1-ata-1.0\",\"PATHS\":[\"/dev/sda\",\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad\",\"/dev/disk/by-path/pci-0000:00:01.1-ata-1.0\"],\"DEVNAME\":\"/dev/sda\",\"DEVPATH\":\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda\",\"DEVTYPE\":\"disk\",\"ID_VENDOR\":null,\"ID_MODEL\":\"VBOX_HARDDISK\",\"ID_SERIAL\":\"VBOX_HARDDISK_VB600bbe13-4145b7ad\",\"ID_FS_TYPE\":null,\"ID_PART_ENTRY_NUMBER\":null,\"IML_SIZE\":\"83886080\",\"IML_SCSI_80\":\"SATA     VBOXHARDDISK   VB600bbe13-4145b7ad\",\"IML_SCSI_83\":\"1ATA     VBOX HARDDISK                           VB600bbe13-4145b7ad\",\"IML_IS_RO\":false},\"/devices/pci0000:00/0000:00:14.0/host2/target2:0:1/2:0:1:0/block/sdb\":{\"ACTION\":\"add\",\"MAJOR\":\"8\",\"MINOR\":\"16\",\"DEVLINKS\":\"/dev/disk/by-path/pci-0000:00:14.0-scsi-0:0:1:0\",\"PATHS\":[\"/dev/sdb\",\"/dev/disk/by-path/pci-0000:00:14.0-scsi-0:0:1:0\"],\"DEVNAME\":\"/dev/sdb\",\"DEVPATH\":\"/devices/pci0000:00/0000:00:14.0/host2/target2:0:1/2:0:1:0/block/sdb\",\"DEVTYPE\":\"disk\",\"ID_VENDOR\":\"VBOX\",\"ID_MODEL\":\"HARDDISK\",\"ID_SERIAL\":null,\"ID_FS_TYPE\":null,\"ID_PART_ENTRY_NUMBER\":null,\"IML_SIZE\":\"1048576000\",\"IML_SCSI_80\":null,\"IML_SCSI_83\":null,\"IML_IS_RO\":false},\"/devices/virtual/block/dm-1\":{\"ACTION\":\"add\",\"MAJOR\":\"253\",\"MINOR\":\"1\",\"DEVLINKS\":\"/dev/VolGroup00/LogVol01 /dev/disk/by-id/dm-name-VolGroup00-LogVol01 /dev/disk/by-id/dm-uuid-LVM-O2bUHhajc4BpdGCKOh7H5ptegIZgHJHMjK0YdM5KFs55rFkEc8wfujk3ykqrWqmQ /dev/disk/by-uuid/1ec946b6-cf86-4b10-981e-27930f167507 /dev/mapper/VolGroup00-LogVol01\",\"PATHS\":[\"/dev/dm-1\",\"/dev/VolGroup00/LogVol01\",\"/dev/disk/by-id/dm-name-VolGroup00-LogVol01\",\"/dev/disk/by-id/dm-uuid-LVM-O2bUHhajc4BpdGCKOh7H5ptegIZgHJHMjK0YdM5KFs55rFkEc8wfujk3ykqrWqmQ\",\"/dev/disk/by-uuid/1ec946b6-cf86-4b10-981e-27930f167507\",\"/dev/mapper/VolGroup00-LogVol01\"],\"DEVNAME\":\"/dev/dm-1\",\"DEVPATH\":\"/devices/virtual/block/dm-1\",\"DEVTYPE\":\"disk\",\"ID_VENDOR\":null,\"ID_MODEL\":null,\"ID_SERIAL\":null,\"ID_FS_TYPE\":\"swap\",\"ID_PART_ENTRY_NUMBER\":null,\"IML_SIZE\":\"3145728\",\"IML_SCSI_80\":\"SATA     VBOX HARDDISK   VB600bbe13-4145b7ad\",\"IML_SCSI_83\":\"1ATA     VBOX HARDDISK                           VB600bbe13-4145b7ad\",\"IML_IS_RO\":false},\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda/sda2\":{\"ACTION\":\"add\",\"MAJOR\":\"8\",\"MINOR\":\"2\",\"DEVLINKS\":\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad-part2 /dev/disk/by-path/pci-0000:00:01.1-ata-1.0-part2 /dev/disk/by-uuid/7a0b4526-ffc0-4a51-9a43-d701c30d7c4f\",\"PATHS\":[\"/dev/sda2\",\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad-part2\",\"/dev/disk/by-path/pci-0000:00:01.1-ata-1.0-part2\",\"/dev/disk/by-uuid/7a0b4526-ffc0-4a51-9a43-d701c30d7c4f\"],\"DEVNAME\":\"/dev/sda2\",\"DEVPATH\":\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda/sda2\",\"DEVTYPE\":\"partition\",\"ID_VENDOR\":null,\"ID_MODEL\":\"VBOX_HARDDISK\",\"ID_SERIAL\":\"VBOX_HARDDISK_VB600bbe13-4145b7ad\",\"ID_FS_TYPE\":\"xfs\",\"ID_PART_ENTRY_NUMBER\":2,\"IML_SIZE\":\"2097152\",\"IML_SCSI_80\":\"SATA    VBOX HARDDISK   VB600bbe13-4145b7ad\",\"IML_SCSI_83\":\"1ATA     VBOX HARDDISK                           VB600bbe13-4145b7ad\",\"IML_IS_RO\":false},\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda/sda3\":{\"ACTION\":\"add\",\"MAJOR\":\"8\",\"MINOR\":\"3\",\"DEVLINKS\":\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad-part3 /dev/disk/by-id/lvm-pv-uuid-eeiAG8-uOsg-ZBhz-38a3-2sn8-ihtm-MWGCsH /dev/disk/by-path/pci-0000:00:01.1-ata-1.0-part3\",\"PATHS\":[\"/dev/sda3\",\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad-part3\",\"/dev/disk/by-id/lvm-pv-uuid-eeiAG8-uOsg-ZBhz-38a3-2sn8-ihtm-MWGCsH\",\"/dev/disk/by-path/pci-0000:00:01.1-ata-1.0-part3\"],\"DEVNAME\":\"/dev/sda3\",\"DEVPATH\":\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda/sda3\",\"DEVTYPE\":\"partition\",\"ID_VENDOR\":null,\"ID_MODEL\":\"VBOX_HARDDISK\",\"ID_SERIAL\":\"VBOX_HARDDISK_VB600bbe13-4145b7ad\",\"ID_FS_TYPE\":\"LVM2_member\",\"ID_PART_ENTRY_NUMBER\":3,\"IML_SIZE\":\"81784832\",\"IML_SCSI_80\":\"SATA     VBOX HARDDISK   VB600bbe13-4145b7ad\",\"IML_SCSI_83\":\"1ATA     VBOX HARDDISK                           VB600bbe13-4145b7ad\",\"IML_IS_RO\":false},\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda/sda1\":{\"ACTION\":\"add\",\"MAJOR\":\"8\",\"MINOR\":\"1\",\"DEVLINKS\":\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad-part1 /dev/disk/by-path/pci-0000:00:01.1-ata-1.0-part1\",\"PATHS\":[\"/dev/sda1\",\"/dev/disk/by-id/ata-VBOX_HARDDISK_VB600bbe13-4145b7ad-part1\",\"/dev/disk/by-path/pci-0000:00:01.1-ata-1.0-part1\"],\"DEVNAME\":\"/dev/sda1\",\"DEVPATH\":\"/devices/pci0000:00/0000:00:01.1/ata1/host0/target0:0:0/0:0:0:0/block/sda/sda1\",\"DEVTYPE\":\"partition\",\"ID_VENDOR\":null,\"ID_MODEL\":\"VBOX_HARDDISK\",\"ID_SERIAL\":\"VBOX_HARDDISK_VB600bbe13-4145b7ad\",\"ID_FS_TYPE\":null,\"ID_PART_ENTRY_NUMBER\":1,\"IML_SIZE\":\"2048\",\"IML_SCSI_80\":\"SATA     VBOX HARDDISK   VB600bbe13-4145b7ad\",\"IML_SCSI_83\":\"1ATA     VBOX HARDDISK                           VB600bbe13-4145b7ad\",\"IML_IS_RO\":false}}"
+      let expectedDevices = "{
+  \"/devices/pci0000:00/0000:00:0d.0/ata4/host3/target3:0:0/3:0:0:0/block/sdb\": {
+    \"ACTION\": \"add\",
+    \"MAJOR\": \"8\",
+    \"MINOR\": \"16\",
+    \"DEVLINKS\": \"/dev/disk/by-id/ata-VBOX_HARDDISK_081118FC1221NCJ6G8GG /dev/disk/by-path/pci-0000:00:0d.0-ata-2.0\",
+    \"PATHS\": [
+      \"/dev/sdb\",
+      \"/dev/disk/by-id/ata-VBOX_HARDDISK_081118FC1221NCJ6G8GG\",
+      \"/dev/disk/by-path/pci-0000:00:0d.0-ata-2.0\"
+    ],
+    \"DEVNAME\": \"/dev/sdb\",
+    \"DEVPATH\": \"/devices/pci0000:00/0000:00:0d.0/ata4/host3/target3:0:0/3:0:0:0/block/sdb\",
+    \"DEVTYPE\": \"disk\",
+    \"ID_VENDOR\": null,
+    \"ID_MODEL\": \"VBOX_HARDDISK\",
+    \"ID_SERIAL\": \"VBOX_HARDDISK_081118FC1221NCJ6G8GG\",
+    \"ID_FS_TYPE\": null,
+    \"ID_PART_ENTRY_NUMBER\": null,
+    \"IML_SIZE\": \"1048576000\",
+    \"IML_SCSI_80\": \"SATA     VBOX HARDDISK   081118FC1221NCJ6G8GG\",
+    \"IML_SCSI_83\": \"1ATA     VBOX HARDDISK                           081118FC1221NCJ6G8GG\",
+    \"IML_IS_RO\": false
+  },
+  \"/devices/pci0000:00/0000:00:0d.0/ata3/host2/target2:0:0/2:0:0:0/block/sda\": {
+    \"ACTION\": \"add\",
+    \"MAJOR\": \"8\",
+    \"MINOR\": \"0\",
+    \"DEVLINKS\": \"/dev/disk/by-id/ata-VBOX_HARDDISK_091118FC1221NCJ6G8GG /dev/disk/by-path/pci-0000:00:0d.0-ata-1.0\",
+    \"PATHS\": [
+      \"/dev/sda\",
+      \"/dev/disk/by-id/ata-VBOX_HARDDISK_091118FC1221NCJ6G8GG\",
+      \"/dev/disk/by-path/pci-0000:00:0d.0-ata-1.0\"
+    ],
+    \"DEVNAME\": \"/dev/sda\",
+    \"DEVPATH\": \"/devices/pci0000:00/0000:00:0d.0/ata3/host2/target2:0:0/2:0:0:0/block/sda\",
+    \"DEVTYPE\": \"disk\",
+    \"ID_VENDOR\": null,
+    \"ID_MODEL\": \"VBOX_HARDDISK\",
+    \"ID_SERIAL\": \"VBOX_HARDDISK_091118FC1221NCJ6G8GG\",
+    \"ID_FS_TYPE\": null,
+    \"ID_PART_ENTRY_NUMBER\": null,
+    \"IML_SIZE\": \"81920000\",
+    \"IML_SCSI_80\": \"SATA     VBOX HARDDISK   091118FC1221NCJ6G8GG\",
+    \"IML_SCSI_83\": \"1ATA     VBOX HARDDISK                           091118FC1221NCJ6G8GG\",
+    \"IML_IS_RO\": false
+  },
+  \"/devices/virtual/block/dm-0\": {
+    \"ACTION\": \"add\",
+    \"MAJOR\": \"253\",
+    \"MINOR\": \"0\",
+    \"DEVLINKS\": \"/dev/centos/root /dev/disk/by-id/dm-name-centos-root /dev/disk/by-id/dm-uuid-LVM-UPOYcrJATlxeJ0Cwqvnt9FPnvHbkkwNyUbg0FbYv7A8ydd9ygP3pxlhPcvhHAcHY /dev/disk/by-uuid/b085695c-789b-44ff-a264-e6a15a3b1900 /dev/mapper/centos-root\",
+    \"PATHS\": [
+      \"/dev/dm-0\",
+      \"/dev/centos/root\",
+      \"/dev/disk/by-id/dm-name-centos-root\",
+      \"/dev/disk/by-id/dm-uuid-LVM-UPOYcrJATlxeJ0Cwqvnt9FPnvHbkkwNyUbg0FbYv7A8ydd9ygP3pxlhPcvhHAcHY\",
+      \"/dev/disk/by-uuid/b085695c-789b-44ff-a264-e6a15a3b1900\",
+      \"/dev/mapper/centos-root\"
+    ],
+    \"DEVNAME\": \"/dev/dm-0\",
+    \"DEVPATH\": \"/devices/virtual/block/dm-0\",
+    \"DEVTYPE\": \"disk\",
+    \"ID_VENDOR\": null,
+    \"ID_MODEL\": null,
+    \"ID_SERIAL\": null,
+    \"ID_FS_TYPE\": \"xfs\",
+    \"ID_PART_ENTRY_NUMBER\": null,
+    \"IML_SIZE\": \"77709312\",
+    \"IML_SCSI_80\": \"SATA     VBOX HARDDISK   091118FC1221NCJ6G8GG\",
+    \"IML_SCSI_83\": \"1ATA     VBOX HARDDISK                           091118FC1221NCJ6G8GG\",
+    \"IML_IS_RO\": false
+  },
+  \"/devices/virtual/block/dm-1\": {
+    \"ACTION\": \"add\",
+    \"MAJOR\": \"253\",
+    \"MINOR\": \"1\",
+    \"DEVLINKS\": \"/dev/centos/swap /dev/disk/by-id/dm-name-centos-swap /dev/disk/by-id/dm-uuid-LVM-UPOYcrJATlxeJ0Cwqvnt9FPnvHbkkwNydHbfPWJwbStUS3q2PuHEsXmZtuBPXafD /dev/disk/by-uuid/a9a3d277-a0e9-4c85-b550-ed3bf2eec439 /dev/mapper/centos-swap\",
+    \"PATHS\": [
+      \"/dev/dm-1\",
+      \"/dev/centos/swap\",
+      \"/dev/disk/by-id/dm-name-centos-swap\",
+      \"/dev/disk/by-id/dm-uuid-LVM-UPOYcrJATlxeJ0Cwqvnt9FPnvHbkkwNydHbfPWJwbStUS3q2PuHEsXmZtuBPXafD\",
+      \"/dev/disk/by-uuid/a9a3d277-a0e9-4c85-b550-ed3bf2eec439\",
+      \"/dev/mapper/centos-swap\"
+    ],
+    \"DEVNAME\": \"/dev/dm-1\",
+    \"DEVPATH\": \"/devices/virtual/block/dm-1\",
+    \"DEVTYPE\": \"disk\",
+    \"ID_VENDOR\": null,
+    \"ID_MODEL\": null,
+    \"ID_SERIAL\": null,
+    \"ID_FS_TYPE\": \"swap\",
+    \"ID_PART_ENTRY_NUMBER\": null,
+    \"IML_SIZE\": \"2097152\",
+    \"IML_SCSI_80\": \"SATA     VBOX HARDDISK   091118FC1221NCJ6G8GG\",
+    \"IML_SCSI_83\": \"1ATA     VBOX HARDDISK                           091118FC1221NCJ6G8GG\",
+    \"IML_IS_RO\": false
+  },
+  \"/devices/pci0000:00/0000:00:0d.0/ata3/host2/target2:0:0/2:0:0:0/block/sda/sda2\": {
+    \"ACTION\": \"add\",
+    \"MAJOR\": \"8\",
+    \"MINOR\": \"2\",
+    \"DEVLINKS\": \"/dev/disk/by-id/ata-VBOX_HARDDISK_091118FC1221NCJ6G8GG-part2 /dev/disk/by-id/lvm-pv-uuid-RkDvOo-Lp2v-X8ih-Tli5-BrYx-TmSG-zp5pyc /dev/disk/by-path/pci-0000:00:0d.0-ata-1.0-part2\",
+    \"PATHS\": [
+      \"/dev/sda2\",
+      \"/dev/disk/by-id/ata-VBOX_HARDDISK_091118FC1221NCJ6G8GG-part2\",
+      \"/dev/disk/by-id/lvm-pv-uuid-RkDvOo-Lp2v-X8ih-Tli5-BrYx-TmSG-zp5pyc\",
+      \"/dev/disk/by-path/pci-0000:00:0d.0-ata-1.0-part2\"
+    ],
+    \"DEVNAME\": \"/dev/sda2\",
+    \"DEVPATH\": \"/devices/pci0000:00/0000:00:0d.0/ata3/host2/target2:0:0/2:0:0:0/block/sda/sda2\",
+    \"DEVTYPE\": \"partition\",
+    \"ID_VENDOR\": null,
+    \"ID_MODEL\": \"VBOX_HARDDISK\",
+    \"ID_SERIAL\": \"VBOX_HARDDISK_091118FC1221NCJ6G8GG\",
+    \"ID_FS_TYPE\": \"LVM2_member\",
+    \"ID_PART_ENTRY_NUMBER\": 2,
+    \"IML_SIZE\": \"79820800\",
+    \"IML_SCSI_80\": \"SATA     VBOX HARDDISK   091118FC1221NCJ6G8GG\",
+    \"IML_SCSI_83\": \"1ATA     VBOX HARDDISK                           091118FC1221NCJ6G8GG\",
+    \"IML_IS_RO\": false
+  },
+  \"/devices/pci0000:00/0000:00:0d.0/ata3/host2/target2:0:0/2:0:0:0/block/sda/sda1\": {
+    \"ACTION\": \"add\",
+    \"MAJOR\": \"8\",
+    \"MINOR\": \"1\",
+    \"DEVLINKS\": \"/dev/disk/by-id/ata-VBOX_HARDDISK_091118FC1221NCJ6G8GG-part1 /dev/disk/by-path/pci-0000:00:0d.0-ata-1.0-part1 /dev/disk/by-uuid/76eef891-8102-4ba3-b1af-6070378aefc8\",
+    \"PATHS\": [
+      \"/dev/sda1\",
+      \"/dev/disk/by-id/ata-VBOX_HARDDISK_091118FC1221NCJ6G8GG-part1\",
+      \"/dev/disk/by-path/pci-0000:00:0d.0-ata-1.0-part1\",
+      \"/dev/disk/by-uuid/76eef891-8102-4ba3-b1af-6070378aefc8\"
+    ],
+    \"DEVNAME\": \"/dev/sda1\",
+    \"DEVPATH\": \"/devices/pci0000:00/0000:00:0d.0/ata3/host2/target2:0:0/2:0:0:0/block/sda/sda1\",
+    \"DEVTYPE\": \"partition\",
+    \"ID_VENDOR\": null,
+    \"ID_MODEL\": \"VBOX_HARDDISK\",
+    \"ID_SERIAL\": \"VBOX_HARDDISK_091118FC1221NCJ6G8GG\",
+    \"ID_FS_TYPE\": \"xfs\",
+    \"ID_PART_ENTRY_NUMBER\": 1,
+    \"IML_SIZE\": \"2097152\",
+    \"IML_SCSI_80\": \"SATA     VBOX HARDDISK   091118FC1221NCJ6G8GG\",
+    \"IML_SCSI_83\": \"1ATA     VBOX HARDDISK                           091118FC1221NCJ6G8GG\",
+    \"IML_IS_RO\": false
+  }
+}"
       let expectedDevicesObj = JSON.parse expectedDevices
       expect.Invoke(startResult).toEqual(expectedDevicesObj)
   ]

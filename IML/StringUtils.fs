2bd5d936beaf9216bd8a8324addebb63d4e940f3
Tom Nabarro <tom.Nabarro@Outlook.com>
2017-11-16 16:55:17 +0000

    bump coverage statistics
    Signed-off-by: Tom Nabarro <tom.Nabarro@Outlook.com>

--- IML/StringUtils.fs
+++ IML/StringUtils.fs
@@ -1,6 +1,4 @@
 module IML.StringUtils
-    let concat (x:string) y = x + y
     let split (x:char []) (s:string) = s.Split(x)
     let trim (y:string) = y.Trim()
-    let startsWith (x:string) y = x.StartsWith(y)
     let emptyStrToNone x = if x = "" then None else Some(x)

a82f7199c7ef0fcda42900c8a61ad47c93054bf2
Joe Grund <joe.grund@intel.com>
2018-01-08 11:47:29 -0500

    Implement streaming
    We want the device-scanner to be push based.
    
    To do this, we can remove the limitation of closing the
    connection from each transaction.
    
    Instead, we can leave that up to the consumer, and
    write back to any sockets still open.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- IML/DeviceScannerDaemon/Server.fs
+++ IML/DeviceScannerDaemon/Server.fs
@@ -5,33 +5,43 @@
 module IML.DeviceScannerDaemon.Server
 
 open Fable.Import.Node
-open Fable.Import.Node.PowerPack
 open Fable.Import.Node.PowerPack.Stream
 open Fable.Import.JS
 open Fable.Core.JsInterop
 open IML.DeviceScannerDaemon.Handlers
-open NodeHelpers
+
+
+let mutable conns = []
 
 let serverHandler (c:Net.Socket):unit =
+  conns <- c :: conns
+
+  let removeConn () =
+    Fable.Import.JS.console.error(List.length conns)
+
+    List.filter (fun x -> c = x) conns
+
+  let writeConns x =
+    List.filter (fun (x:Net.Socket) -> not (!!x?destroyed)) conns
+      |> List.iter (fun c -> Writable.write x c |> ignore)
+
   c
-    |> LineDelimitedJsonStream.getJsonStream()
-    |> error (fun (e:Error) ->
+    |> Readable.onEnd (fun () -> conns <- removeConn())
+    |> LineDelimitedJson.create()
+    |> Readable.onError (fun (e:Error) ->
       console.error ("Unable to parse message " + e.message)
+      conns <- removeConn()
       c.``end``()
     )
-    |> iter (dataHandler (``end`` c))
+    |> map dataHandler
+    |> map (toJson >> buffer.Buffer.from >> Ok)
+    |> iter writeConns
     |> ignore
 
-let opts = createEmpty<Net.CreateServerOptions>
-opts.allowHalfOpen <- Some true
+let private server = net.createServer(serverHandler)
 
-let private server = Net.createServer(opts, serverHandler)
-let private r e =
-  e
-  |> raise
-  |> ignore
-
-server.on("error", r)
+server
+  |> Readable.onError raise
   |> ignore
 
 let private fd = createEmpty<Net.Fd>

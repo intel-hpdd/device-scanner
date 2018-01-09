834fb2ec5882f51ae7edab9c3b720fc5fa95b5dc
Tom Nabarro <tom.nabarro@outlook.com>
2018-01-09 17:10:29 +0000

    resolve external dependency
    Signed-off-by: Tom Nabarro <tom.nabarro@outlook.com>

--- IML/EventListener/rollup-config.js
+++ IML/EventListener/rollup-config.js
@@ -2,7 +2,7 @@ import baseConfig from '../../base-rollup-config.js';
 
 export default Object.assign({}, baseConfig, {
   input: 'IML/EventListener/Listener.fs',
-  external: ['net', 'buffer'],
+  external: ['net', 'buffer', 'stream'],
   output: {
     banner: '#!/usr/bin/env node',
     file: './dist/event-listener/event-listener',

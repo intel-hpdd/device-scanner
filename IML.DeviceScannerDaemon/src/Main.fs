b8e450158fc8633d3e638ba1034db07a1f385a84
Joe Grund <joe.grund@intel.com>
2018-02-27 11:26:32 -0500

    update connection handling
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- IML.DeviceScannerDaemon/src/Main.fs
+++ IML.DeviceScannerDaemon/src/Main.fs
@@ -4,31 +4,31 @@
 
 module IML.DeviceScannerDaemon.Server
 
-open Fable.Import
 open Fable.Import.Node
-open Buffer
 open PowerPack.Stream
 open Fable.Core.JsInterop
 open Handlers
 open IML.Types.CommandTypes
 
-let private parser (x:Buffer) =
+let private parser x =
   try
-    x.toString()
+    x
       |> ofJson<Command>
       |> Ok
   with
     | ex ->
       Error ex
-
 let serverHandler (c:Net.Socket):unit =
-  Connections.addConn c
-
   c
-    |> Readable.onEnd (Connections.removeConn c)
+    |> LineDelimited.create()
     |> map parser
-    |> Readable.onError (fun (e:JS.Error) ->
-      eprintfn "Unable to parse message %s" e.message
+    |> Readable.onError (fun (e:exn) ->
+      eprintfn "Unable to parse message %s" e.Message
+      c.``end``()
+    )
+    |> tap (function
+      | Info -> Connections.addConn (Connections.Persistent c)
+      | _ -> Connections.addConn (Connections.Ephemeral c)
     )
     |> map backCompatHandler
     |> map (
@@ -43,8 +43,11 @@ let serverHandler (c:Net.Socket):unit =
 let private fd = createEmpty<Net.Fd>
 fd.fd <- 3
 
+let opts = createEmpty<Net.CreateServerOptions>
+opts.allowHalfOpen <- Some true
+
 net
-  .createServer(serverHandler)
+  .createServer(opts, serverHandler)
   .listen(fd)
     |> Readable.onError raise
     |> ignore

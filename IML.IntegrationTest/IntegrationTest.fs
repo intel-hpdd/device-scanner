de564f4314acdf0d35905bf04d89c2cc021f1c8c
Will Johnson <william.c.johnson@intel.com>
2018-03-16 13:48:42 -0400

    Integrate udev serializer and 'remove device' integration test
    The integration tests will be working with a mixture of data. Most of
    the data will be invariant; However, some of the data is variable and
    will change on each run. Multipath devices are a perfect example of
    this, as well as the root and swap partitions, which are mapped
    according to the device mapper. To handle variable data, a serializer
    will be used to transform incoming data. Regular expressions will be
    used to match the keys on the udev object and the variable portions of
    the data will then be transformed such that the data will match the
    corresponding snapshot.
    
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- IML.IntegrationTest/IntegrationTest.fs
+++ IML.IntegrationTest/IntegrationTest.fs
@@ -4,16 +4,20 @@
 
 module IML.IntegrationTest.IntegrationTest
 
-open Fable.Import.Jest.Matchers
 open Fable.Core.JsInterop
-open Fable.PowerPack
 open IML.StatefulPromise.StatefulPromise
 open IML.IntegrationTestFramework.IntegrationTestFramework
 
+open Fable.Import
 open Fable.Import.Jest
+open Matchers
 open Fable.Import.Node
 open Fable.Import.Node.PowerPack
-open Fable.PowerPack.Json
+open Fable.PowerPack
+open Json
+
+let settle () =
+  cmd "udevadm settle"
 
 let scannerInfo =
   pipeToShellCmd "echo '\"Info\"'" "socat - UNIX-CONNECT:/var/run/device-scanner.sock"
@@ -32,18 +36,43 @@ let resultOutput: StatefulResult<State, Out, Err> -> string = function
   | Ok ((Stdout(r), _), _) -> r
   | Error (e) -> failwithf "Command failed: %A" !!e
 
+let rbScanForDisk (): RollbackState -> RollbackCommandState =
+  rbCmd "for host in `ls /sys/class/scsi_host`; do echo \"- - -\" > /sys/class/scsi_host/$host/scan; done"
+
+let rbSetDeviceState (name:string) (state:string): RollbackState -> RollbackCommandState =
+  rbCmd (sprintf "echo \"%s\" > /sys/block/%s/device/state" state name)
+
+let setDeviceState (name:string) (state:string): State -> JS.Promise<CommandResult<Out, Err>> =
+  cmd (sprintf "echo \"%s\" > /sys/block/%s/device/state" state name)
+
+let deleteDevice (name:string): State -> JS.Promise<CommandResult<Out, Err>> =
+  cmd (sprintf "echo \"1\" > /sys/block/%s/device/delete" name)
+
+let scanForDisk () =
+  cmd "for host in `ls /sys/class/scsi_host`; do echo \"- - -\" > /sys/class/scsi_host/$host/scan; done"
+
+let matchResultToSnapshot (r:StatefulResult<State, Out, Err>, _): unit =
+  let json =
+    r
+      |> resultOutput
+      |> unwrapDeviceData
+      |> toJson
+      |> buffer.Buffer.from
+
+  toMatchSnapshot json
+
 testAsync "info event" <| fun () ->
   command {
     return! scannerInfo
   }
   |> startCommand "Info Event"
-  |> Promise.map (fun (r, _) ->
-      let json =
-        r
-          |> resultOutput
-          |> unwrapDeviceData
-          |> toJson
-          |> buffer.Buffer.from
+  |> Promise.map matchResultToSnapshot
 
-      toMatchSnapshot json
-  )
+testAsync "remove a device" <| fun () ->
+  command {
+    do! (setDeviceState "sdc" "offline") >> rollbackError (rbSetDeviceState "sdc" "running") >> ignoreCmd
+    do! (deleteDevice "sdc") >> rollback (rbScanForDisk ()) >> ignoreCmd
+    return! scannerInfo
+  }
+  |> startCommand "removing a device"
+  |> Promise.map matchResultToSnapshot
---
+++ udev-serializer.js
@@ -0,0 +1,197 @@
+const buffer = require('buffer');
+
+const normalizeVariablePaths = (o, replacements) => {
+  return {
+    Array: o.Array.map(({ String }) => {
+      const matcher = replacements.find(([matcher]) => {
+        return matcher.test(String);
+      });
+
+      if (matcher) {
+        const [, replacement] = matcher;
+        return { String: replacement };
+      } else {
+        return { String };
+      }
+    })
+  };
+};
+
+const transformEntry = (key, fn, data) => {
+  return {
+    [key]: {
+      Object: data.Object.map(([k, v]) => {
+        return fn(k, v);
+      })
+    }
+  };
+};
+
+const transformPaths = (key, replacements, obj) =>
+  transformEntry(
+    key,
+    (k, v) => {
+      if (k === 'PATHS') return [k, normalizeVariablePaths(v, replacements)];
+      else return [k, v];
+    },
+    obj
+  );
+
+const transformDevPath = (key, obj) =>
+  transformEntry(
+    key,
+    (k, v) => {
+      if (k === 'DEVPATH') return [k, { String: key }];
+      else return [k, v];
+    },
+    obj
+  );
+
+const transformDMUUID = (key, uuid, obj) =>
+  transformEntry(
+    key,
+    (k, v) => {
+      if (k === 'DM_UUID') return [k, { String: uuid }];
+      else return [k, v];
+    },
+    obj
+  );
+
+const transformItem = (keyRegex, transforms, data) => {
+  return Object.keys(data).reduce((acc, key) => {
+    const obj = Object.assign({}, data[key]);
+    if (keyRegex.test(key))
+      acc = Object.assign(
+        {},
+        acc,
+        transforms.reduce((o, fn) => fn(key, o), obj)
+      );
+    else acc[key] = obj;
+
+    return acc;
+  }, {});
+};
+
+const transformEntries = (transforms, data) =>
+  transforms.reduce(
+    (acc, [keyRegex, propTransforms]) =>
+      transformItem(keyRegex, propTransforms, acc),
+    data
+  );
+
+module.exports = {
+  print(x, serialize) {
+    const val = x.toString();
+    const data = JSON.parse(val);
+
+    const newData = transformEntries(
+      [
+        [
+          /.+\/block\/sda\/sda1/,
+          [
+            (key, obj) =>
+              transformPaths(
+                key,
+                [
+                  [
+                    /\/dev\/disk\/by-uuid/,
+                    '/dev/disk/by-uuid/74b3fabd-dbf5-4cc0-a967-2c12f8113fa6'
+                  ]
+                ],
+                obj
+              )
+          ]
+        ],
+        [
+          /.+\/block\/sda\/sda2/,
+          [
+            (key, obj) =>
+              transformPaths(
+                key,
+                [
+                  [
+                    /\/dev\/disk\/by-id\/lvm-pv-uuid/,
+                    '/dev/disk/by-id/lvm-pv-uuid-tVAIF5-nJY7-oKaP-wLQO-OIXp-Ggwn-0F9F70'
+                  ]
+                ],
+                obj
+              )
+          ]
+        ],
+        [
+          /\/devices\/virtual\/block\/dm-0/,
+          [
+            (key, obj) =>
+              transformPaths(
+                key,
+                [
+                  [
+                    /\/dev\/disk\/by-id\/dm-uuid-LVM/,
+                    '/dev/disk/by-id/dm-uuid-LVM-FpAffE3HiAwoAvd81g8dBirIbkC3Ogu58AIfMI4SXo1AodrQkxuO2yuvd2JOPi5j'
+                  ],
+                  [
+                    /\/dev\/disk\/by-uuid\//,
+                    '/dev/disk/by-uuid/45252d52-d8d6-468e-aaa0-c117b042944a'
+                  ]
+                ],
+                obj
+              )
+          ]
+        ],
+        [
+          /\/devices\/virtual\/block\/dm-1/,
+          [
+            (key, obj) =>
+              transformPaths(
+                key,
+                [
+                  [
+                    /\/dev\/disk\/by-id\/dm-uuid-LVM/,
+                    '/dev/disk/by-id/dm-uuid-LVM-FpAffE3HiAwoAvd81g8dBirIbkC3Ogu50snmtLIcILKydXKz4EgqhgxR5Tf013zE'
+                  ],
+                  [
+                    /\/dev\/disk\/by-uuid\//,
+                    '/dev/disk/by-uuid/4af3b3ab-356b-490b-9aab-2e9786804b79'
+                  ]
+                ],
+                obj
+              )
+          ]
+        ],
+        [
+          /\/devices\/virtual\/block\/dm-0/,
+          [
+            (key, obj) =>
+              transformDMUUID(
+                key,
+                'LVM-FpAffE3HiAwoAvd81g8dBirIbkC3Ogu58AIfMI4SXo1AodrQkxuO2yuvd2JOPi5j',
+                obj
+              )
+          ]
+        ],
+        [
+          /\/devices\/virtual\/block\/dm-1/,
+          [
+            (key, obj) =>
+              transformDMUUID(
+                key,
+                'LVM-FpAffE3HiAwoAvd81g8dBirIbkC3Ogu50snmtLIcILKydXKz4EgqhgxR5Tf013zE',
+                obj
+              )
+          ]
+        ]
+      ],
+      data
+    );
+
+    try {
+      return serialize(JSON.stringify(newData, null, 2));
+    } catch (e) {
+      return serialize(val);
+    }
+  },
+
+  test(x) {
+    return x && buffer.Buffer.isBuffer(x);
+  }
+};

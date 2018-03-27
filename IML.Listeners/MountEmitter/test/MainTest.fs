cde8f7aba9ab4dc8bf3ccf28d37e8e8536634058
Joe Grund <joe.grund@intel.com>
2018-03-27 17:02:15 -0400

    make test case async
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- IML.Listeners/MountEmitter/test/MainTest.fs
+++ IML.Listeners/MountEmitter/test/MainTest.fs
@@ -6,14 +6,14 @@ open Fable.Import.Node.PowerPack.Stream
 open IML.MountEmitter
 open IML.Types.CommandTypes
 
-test "header then data" <| fun () ->
+testAsync "header then data" <| fun () ->
   streams {
     yield "ACTION TARGET SOURCE FSTYPE OPTIONS\n"
 
     yield "mount      /mnt/fs-OST0002 /dev/sdd lustre ro\n"
   }
     |> transform
-    |> iter (fun xs ->
+    |> tap (fun xs ->
       xs == Command.MountCommand (Mount (Mount.MountPoint "/mnt/fs-OST0002", Mount.BdevPath "/dev/sdd", Mount.FsType "lustre", Mount.Options "ro"))
     )
-    |> ignore
+    |> Util.streamToPromise

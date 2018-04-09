a77005ffdb4c1ec6f8a8964d599698c837370e70
Will Johnson <william.c.johnson@intel.com>
2018-04-09 13:22:39 -0400

    Take the first 32 characters from the suffix.
    Signed-off-by: Will Johnson <william.c.johnson@intel.com>

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 vagrant destroy -f
-export NAME_SUFFIX=$(pwd | sha256sum)
+export NAME_SUFFIX=$(pwd | sha256sum | head -c 32)
 vagrant up device-scanner$NAME_SUFFIX test$NAME_SUFFIX
 vagrant destroy -f

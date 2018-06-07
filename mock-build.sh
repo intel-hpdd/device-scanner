799af6b53ac52975b5e0ea05bc77ba880ec2c0a8
Joe Grund <joe.grund@intel.com>
2018-06-07 12:25:15 -0400

    Use new version of dotnet-core, and switch Copr build type
    Switch to the new version of dotnet core, which thankfully
    doesn't require us to use scl anymore!
    
    Also, switch the Copr build type to make_srpm,
    which should give us a nice way to build branches
    on copr, and build both from a registry and locally.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- mock-build.sh
+++ mock-build.sh
@@ -1,41 +1,15 @@
 #!/bin/bash -xe
 
-sed -i "s/\(config_opts\['chroot_setup_cmd']\) = '\(.*\)'/\1 = '\2 scl-utils-build'/" /etc/mock/default.cfg
-
-ed <<"EOF" /etc/mock/default.cfg
-$i
-
-[copr-be.cloud.fedoraproject.org_results_managerforlustre_manager-for-lustre_epel-7-x86_64_]
-name=added from: https://copr-be.cloud.fedoraproject.org/results/managerforlustre/manager-for-lustre-devel/epel-7-x86_64/
-baseurl=https://copr-be.cloud.fedoraproject.org/results/managerforlustre/manager-for-lustre-devel/epel-7-x86_64/
-enabled=1
-
-[dotnet]
-name=CentOS-7 - DotNet
-baseurl=http://mirror.centos.org/centos/$releasever/dotnet/$basearch/
-enabled=1
-
-[mono-centos7-stable]
-name=mono-centos7-stable
-baseurl=http://download.mono-project.com/repo/centos7-stable/
-enabled=1
-
-.
-w
-q
-EOF
-
-chown -R mockbuild:mock /builddir
 
 cd /builddir/
-git clean -dfx
-rpkg make-source
-RELEASE=$(git rev-list HEAD | wc -l)
 
+rpmlint /builddir/device-scanner.spec
+yum install -y dnf
+make -f /builddir/.copr/Makefile srpm spec="/builddir/device-scanner.spec" outdir="/builddir/"
+
+chown -R mockbuild:mock /builddir
 su - mockbuild <<EOF
 set -xe
 cd /builddir/
-rpmlint \$PWD *.spec
-rpmbuild -bs --define epel\ 1 --define package_release\ $RELEASE --define _srcrpmdir\ \$PWD --define _sourcedir\ \$PWD *.spec
-mock iml-device-scanner-*.src.rpm -v --resultdir="/builddir" --rpmbuild-opts="--define package_release\ $RELEASE" --enable-network
+mock iml-device-scanner-*.src.rpm --resultdir="/builddir" --enable-network
 EOF

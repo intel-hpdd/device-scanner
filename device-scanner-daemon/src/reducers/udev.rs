81dc2345b6325fdfcbf8b442502bf1973bc85a2a
Joe Grund <jgrund@whamcloud.io>
2018-11-14 13:27:15 -0500

    add copyright and fix travis deploy
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- device-scanner-daemon/src/reducers/udev.rs
+++ device-scanner-daemon/src/reducers/udev.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 use device_types::{state, udev::UdevCommand};
 
 /// Mutably updates the Udev portion of the device map in response to `UdevCommand`s.
--- device-types/src/lib.rs
+++ device-types/src/lib.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 #[macro_use]
 extern crate serde_derive;
 

63b11e22a955ac43b5766d71304e7fbb7d7e226b
Joe Grund <jgrund@whamcloud.io>
2018-12-04 09:38:37 -0500

    add copyrights
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- device-aggregator/src/dag.rs
+++ device-aggregator/src/dag.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 DDN. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 use aggregator_error;
 use device_types::devices::{self, AsParent, Device, Parent};
 use std::path::PathBuf;

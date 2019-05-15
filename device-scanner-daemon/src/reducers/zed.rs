3fff1428c7c1e7a6daeb3e7188e8028e3e211cfc
Joe Grund <jgrund@whamcloud.io>
2019-05-15 13:09:59 -0500

    Ensure zpool is returned after being taken
    Fixes #268
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- device-scanner-daemon/src/reducers/zed.rs
+++ device-scanner-daemon/src/reducers/zed.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2018 DDN. All rights reserved.
+// Copyright (c) 2019 DDN. All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
@@ -20,15 +20,13 @@ pub fn take_pool(zed_events: &mut state::ZedEvents, guid: u64) -> Result<libzfs_
     })
 }
 
-fn update_prop(name: &str, value: &str, xs: Vec<libzfs_types::ZProp>) -> Vec<libzfs_types::ZProp> {
-    let mut xs: Vec<libzfs_types::ZProp> = xs.into_iter().filter(|z| z.name != name).collect();
+fn update_prop(name: &str, value: &str, xs: &mut Vec<libzfs_types::ZProp>) {
+    xs.retain(|z| z.name != name);
 
     xs.push(libzfs_types::ZProp {
         name: name.to_string(),
         value: value.to_string(),
     });
-
-    xs
 }
 
 fn guid_to_u64(guid: zpool::Guid) -> Result<u64> {
@@ -68,7 +66,7 @@ pub fn update_zed_events(
 
             pool.datasets = ds;
 
-            Ok(zed_events)
+            Ok(zed_events.update(pool.guid, pool))
         }
         PoolCommand::RemoveDataset(guid, zfs::Name(name)) => {
             let guid = guid_to_u64(guid)?;
@@ -81,36 +79,36 @@ pub fn update_zed_events(
                 .filter(|d| d.name != name)
                 .collect();
 
-            Ok(zed_events)
+            Ok(zed_events.update(pool.guid, pool))
         }
         PoolCommand::SetZpoolProp(guid, prop::Key(key), prop::Value(value)) => {
             let guid = guid_to_u64(guid)?;
 
             let mut pool = take_pool(&mut zed_events, guid)?;
 
-            pool.props = update_prop(&key, &value, pool.props);
+            update_prop(&key, &value, &mut pool.props);
 
-            Ok(zed_events)
+            Ok(zed_events.update(pool.guid, pool))
         }
         PoolCommand::SetZfsProp(guid, zfs::Name(name), prop::Key(key), prop::Value(value)) => {
             let guid = guid_to_u64(guid)?;
 
             fn get_dataset_in_pool(
-                pool: libzfs_types::Pool,
+                pool: &mut libzfs_types::Pool,
                 name: String,
-            ) -> Result<libzfs_types::Dataset> {
+            ) -> Result<&mut libzfs_types::Dataset> {
                 pool.datasets
-                    .into_iter()
+                    .iter_mut()
                     .find(|d| d.name == name)
                     .ok_or_else(|| Error::LibZfsError(libzfs_types::LibZfsError::ZfsNotFound(name)))
             }
 
-            let pool = take_pool(&mut zed_events, guid)?;
-            let mut dataset = get_dataset_in_pool(pool, name)?;
+            let mut pool = take_pool(&mut zed_events, guid)?;
+            let dataset = get_dataset_in_pool(&mut pool, name)?;
 
-            dataset.props = update_prop(&key, &value, dataset.props);
+            update_prop(&key, &value, &mut dataset.props);
 
-            Ok(zed_events)
+            Ok(zed_events.update(pool.guid, pool))
         }
     }
 }

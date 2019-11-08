115c4398a43c5f08708f3ba027e422ebe7807e53
Joe Grund <jgrund@whamcloud.io>
2019-11-08 16:45:53 -0500

    Always send ZFS state on startup (#296)
    * Always send ZFS state on startup
    
    When starting zed-populator, ensure we always send the current state.
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>
    
    * updates
    
    Signed-off-by: Joe Grund <jgrund@whamcloud.io>

--- zed-enhancer/src/main.rs
+++ zed-enhancer/src/main.rs
@@ -10,14 +10,16 @@
 //! This crate receives events from device-scanner-zedlets and may enhance them with further data
 //! before passing onwards to device-scanner.
 
+use device_types::zed::ZedCommand;
 use futures::TryStreamExt;
 use std::{
     convert::TryFrom,
     os::unix::{io::FromRawFd, net::UnixListener as NetUnixListener},
 };
 use tokio::net::UnixListener;
+use tokio_net::process::Command;
 use tracing_subscriber::{fmt::Subscriber, EnvFilter};
-use zed_enhancer::processor;
+use zed_enhancer::{handle_zed_commands, processor, send_to_device_scanner};
 
 #[tokio::main(single_thread)]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -27,7 +29,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
-    tracing::info!("Server started");
+    let zfs_loaded = Command::new("/usr/sbin/udevadm")
+        .args(&["info", "--path=/module/zfs"])
+        .output()
+        .await?
+        .status
+        .success();
+
+    if zfs_loaded {
+        tracing::debug!("Sending initial data");
+
+        let pool_command = handle_zed_commands(ZedCommand::Init)?;
+
+        send_to_device_scanner(&pool_command).await?;
+    }
+
+    tracing::info!("Server starting");
 
     let addr = unsafe { NetUnixListener::from_raw_fd(3) };
 

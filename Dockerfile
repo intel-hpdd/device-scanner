cbae24a5add7d94dc8ca7cc78f07c26bde0d2d95
Will Johnson <wjohnson@whamcloud.com>
2019-11-25 09:54:36 -0500

    - If the PROXY_HOST variable isn't set then select "127.0.0.1" as the host
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,4 @@ WORKDIR /build
 COPY . .
 RUN cargo build -p device-aggregator --release
 
-FROM rust:1.39
-COPY --from=builder /build/target/release/device-aggregator /usr/local/bin
-RUN rm -rf /var/lib/apt/lists/*
-
 CMD ["device-aggregator"]
--- device-aggregator/src/main.rs
+++ device-aggregator/src/main.rs
@@ -13,11 +13,15 @@ use device_types::{devices::Device, message::Message};
 use futures::Future;
 use std::{
     collections::{BTreeMap, HashMap},
-    env,
+    env::{var, VarError},
     net::ToSocketAddrs,
     sync::{Arc, Mutex},
 };
 use warp::Filter;
+
+fn to_local_host(_: VarError) -> Result<String, String> {
+    Ok("127.0.0.1".to_string())
+}
 
 fn main() -> Result<(), aggregator_error::Error> {
     env_logger::builder().default_format_timestamp(false).init();
@@ -31,8 +35,10 @@ fn main() -> Result<(), aggregator_error::Error> {
 
     let log = warp::log("device_aggregator");
 
-    let host = env::var("PROXY_HOST").expect("PROXY_HOST environment variable is required.");
-    let port: u16 = env::var("DEVICE_AGGREGATOR_PORT")
+    let host: String = var("PROXY_HOST")
+        .or_else(to_local_host)
+        .expect("Couldn't parse host.");
+    let port: u16 = var("DEVICE_AGGREGATOR_PORT")
         .expect("DEVICE_AGGREGATOR_PORT environment variable is required.")
         .parse()
         .expect("could not parse DEVICE_AGGREGATOR_PORT to u16");
